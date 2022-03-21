dc3ef12b3e72f1ee0b579cfc0a1a88ccbbe937db
nicolecapuno_11 <colecapuno@gmail.com>
2022-03-21 22:32:04 +0800

    Customized the design, added my own logo, updated the layout of producta, and shopping cart

--- products-list.php
+++ products-list.php
@@ -12,9 +12,13 @@ $customer = new Customer('John Doe', 'john@mail.com');
 <html>
 <head>
     <title>My Cart</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-
+<div id="logo">
+    <img src="fruits.jpg">
+</div>
 <h1>Welcome <?php echo $customer->getName() ?>!</h1>
 <h2>Products</h2>
 <h4>
